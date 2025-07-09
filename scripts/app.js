9ff1b60e93f8ba5a3ca225677a27352cce367ba8
Alijassim18 <iceali927@gmail.com>
2025-07-09 13:32:58 +0300

    ksk

--- scripts/app.js
+++ scripts/app.js
@@ -3,18 +3,46 @@ function init(){
 
     const gridElem=document.querySelector('.grid')
 
+    const cells=[]
+
+    const gridWidth=10
+    const numberOfCells=gridWidth * gridWidth
+
+    let duckPosition = 20 //Can put number between 0to99
+
+    function addDuck(){
+        cells[duckPosition].classList.add('duck')
+    }
+    function removeDuck(){
+        cells[duckPosition].classList.remove('duck')
+    }
+    function play(){
+        setInterval(()=>{
+            removeDuck()
+            duckPosition=Math.floor(Math.random()*numberOfCells)
+            addDuck()
+        },200)
+    }
+
 function createGrid(){
     // for every cell that we require create a div
     // append this cell to our grid
-    for(let i=0;i<100;i++){
+    for(let i=0;i<numberOfCells;i++){
         const cell=document.createElement('div')
+        // cell.classList.add('duck')
+
+
         cell.textContent=i
+        cells.push(cell)
         gridElem.appendChild(cell)
     }
- 
+ console.log(cells)
 
 }
    createGrid()
+   
+   play()
+//    removeDuck()
 }
 
 document.addEventListener('DOMContentLoaded',init)
